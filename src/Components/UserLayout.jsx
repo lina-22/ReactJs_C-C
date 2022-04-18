8cf4b67206e050ad854a9c595d041f6b44d1dbf4
lina-22 <haquefahinur@gmail.com>
2022-04-18 16:45:17 +0200

    AdminLayout and UserLayout set up

--- src/Components/UserLayout.jsx
+++ src/Components/UserLayout.jsx
@@ -2,18 +2,16 @@ import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-function UserLayout () {
-    return ( 
-        <div className="main">
-            <div id="landing">
-                <Navbar/> 
-                <Outlet/>
-                <Footer/>
-
-            </div>
-
-        </div>
-     );
+function UserLayout() {
+  return (
+    <div className="main">
+      <div id="landing">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default  UserLayout;
+export default UserLayout;
