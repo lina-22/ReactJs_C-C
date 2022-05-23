d6324859d15cb22e8a4fcdcbf6a22880e98293be
lina-22 <haquefahinur@gmail.com>
2022-05-23 17:28:30 +0200

    layout setup at App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import { Routes, Route } from "react-router-dom";
 
+// *************************************************
+// **************All Pages For Admin****************
+// *************************************************
 import AdminLayout from "./Components/AdminLayout";
-import UserLayout from "./Components/UserLayout";
-
-import Accueil from "./Pages/User/Accueil";
-import BoutiqueLandingImgaes from "./Pages/User/BoutiqueLandingImgaes";
-import BoutiqueSubSection from "./Pages/User/BoutiqueSubSection";
-
-
-import Panier from "./Pages/User/Group";
-import Propos from "./Pages/User/Propos";
-import Payment from "./Pages/User/payment";
-
 import AdminDashBord from "./Pages/Admin/AdminDashBord";
 import Category from "./Pages/Admin/Category";
 import Product from "./Pages/Admin/Product";
 import ProductAvailable from "./Pages/Admin/ProductAvailable";
+import Reservations from "./Pages/Admin/Reservations";
+// *************************************************
+// **************All Pages For User****************
+// *************************************************
+import UserLayout from "./Components/UserLayout";
+import Accueil from "./Pages/User/Accueil";
+import BoutiqueLandingImgaes from "./Pages/User/BoutiqueLandingImgaes";
+import BoutiqueSubSection from "./Pages/User/BoutiqueSubSection";
+import Carts from "./Pages/User/carts";
+import Panier from "./Pages/User/Group";
+import Payment from "./Pages/User/payment";
+import Propos from "./Pages/User/Propos";
 
+// *************************************************
+// **************All Pages For Auth****************
+// *************************************************
 import Login from "./Pages/Auth_Connexion/Login";
+import Registration from "./Pages/Auth_Connexion/Registration";
 
+// *************************************************
+// **************All Imports For Context****************
+// *************************************************
 import {
   AuthContext,
   CategoryContext,
@@ -27,32 +38,48 @@ import {
   ReservationContext,
   AdminReservationContext,
 } from "./contexts";
-import { useReducer } from "react";
-import { authReducer } from "./reducers/authReducer";
 
-import axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
+// *************************************************
+// **************All Imports For Reducer****************
+// *************************************************
+import { useReducer } from "react";
+import { adminReservationReducer, adminReservationStore } from "./reducers/adminReservationReducer";
+import { authReducer } from "./reducers/authReducer";
 import { categoryReducer, categoryStore } from "./reducers/categoryReducer";
 import { productReducer, productStore } from "./reducers/productReducer";
+import { reservationReducer, reservationStore } from "./reducers/reservationReducer";
 import {
   productAvailableReducer,
   productAvailableStore,
 } from "./reducers/productAvailableReducer";
-import Registration from "./Pages/Auth_Connexion/Registration";
-import { reservationReducer, reservationStore } from "./reducers/reservationReducer";
-import Carts from "./Pages/User/carts";
-import { adminReservationReducer, adminReservationStore } from "./reducers/adminReservationReducer";
-import Reservations from "./Pages/Admin/Reservations";
+
+// *************************************************
+// **************All Imports For React-Bootsrap/CSS****************
+// *************************************************
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
+// *************************************************
+// **************All Imports For Token/axios****************
+// *************************************************
+import axios from "axios";
 
 const token = localStorage.getItem("AccessToken");
 // console.log(token);
 if (token) {
   axios.defaults.headers.common = { Authorization: token };
 }
+// *************************************************
+// **************Function App started***************
+// *************************************************
 
 function App() {
+  const [adminReservationValue, adminReservationDispatch] = useReducer(
+    adminReservationReducer,
+    adminReservationStore
+  );
+
   const [auth, authDispatch] = useReducer(authReducer, {});
 
   const [categoryValue, categoryDispatch] = useReducer(
@@ -60,24 +87,21 @@ function App() {
     categoryStore
   );
 
-  const [productValue, productDispatch] = useReducer(
-    productReducer,
-    productStore
-  );
-
   const [productAvailableValue, productAvailableDispatch] = useReducer(
     productAvailableReducer,
     productAvailableStore
+  );
+
+  const [productValue, productDispatch] = useReducer(
+    productReducer,
+    productStore
   );
 
   const [reservationValue, reservationDispatch] = useReducer(
     reservationReducer,
     reservationStore
   ); 
-  const [adminReservationValue, adminReservationDispatch] = useReducer(
-    adminReservationReducer,
-    adminReservationStore
-  );
+
   return (
     <>
     <AuthContext.Provider value={{ auth, authDispatch }}>
