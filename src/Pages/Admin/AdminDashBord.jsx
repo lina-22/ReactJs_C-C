f7741bd38e02860032aa4daca1c5e75dfca63fb0
lina-22 <haquefahinur@gmail.com>
2022-05-23 19:48:39 +0200

    cleaning the codes and layout setup

--- src/Pages/Admin/AdminDashBord.jsx
+++ src/Pages/Admin/AdminDashBord.jsx
@@ -7,9 +7,12 @@ function AdminDashBord() {
   return (
     <Container className="mx-auto">
     <div>
-      <section className="AdminDashBoardImg">
-        {/* <img src={AdminDashBoardImg} alt="" /> */}
-      </section>
+      {/* <section className="AdminDashBoardImg">
+        <img src={AdminDashBoardImg} alt="" />
+      </section> */}
+      <div className="AdminDroits">
+      <p>Les droits administratifs sont des autorisations accordées par les administrateurs aux utilisateurs qui leur permettent de créer, de supprimer et de modifier des éléments et des paramètres. <br /> Sans droits d'administration, utilisateurs pouvent pas effectuer de nombreuses modifications du système, telles que les produits, la catégorie, les paramètres de disponibilité des produits.</p>
+    </div>
     </div>
     </Container>
   );
